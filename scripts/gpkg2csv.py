7eb65c2b09eded5c1c27429864f4671f0fd0c518
u77932 <neil.symington@ga.gov.au>
2023-06-29 16:02:45 +1000

    added IGS water levels and Warburton 1999 bores

--- scripts/gpkg2csv.py
+++ scripts/gpkg2csv.py
@@ -15,3 +15,9 @@ for lyr in ["MORPH_LithologyLog", "MORPH_BoreLog", "MORPH_ConstructionLog"]:
     gdf.to_csv(os.path.join(outdir, ".".join([lyr, "csv"])),
                index = False)
 
+gdf = gpd.read_file(infile, layer = 'MORPH_WaterLevels')
+gdf = gdf.sort_values(by = ['MORPH_ID', "measuredDate"])
+gdf.to_csv(os.path.join(outdir, ".".join(['MORPH_WaterLevels', "csv"])),
+               index = False)
+
+
